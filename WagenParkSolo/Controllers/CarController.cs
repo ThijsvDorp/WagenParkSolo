845ca15c6c6c6f13151ba0acd9f7170fa4ad516f
Thijs van Dorp <Thijsjuh97@hotmail.com>
2021-06-03 14:16:34 +0200

    Added Carcontroller

---
+++ WagenParkSolo/Controllers/CarController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WagenParkSolo.Models;
+
+namespace WagenParkSolo.Controllers
+{
+    public class CarController : Controller
+    {
+        private WagenparkEntities db = new WagenparkEntities();
+
+        // GET: Car
+        public ActionResult Index()
+        {
+            return View(db.Autoes.ToList());
+        }
+
+        // GET: Car/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auto auto = db.Autoes.Find(id);
+            if (auto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auto);
+        }
+
+        // GET: Car/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Car/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Autonummer,Merk,Kenteken,Model,Brandstof,Kmstand,Kostperdag,Foto")] Auto auto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Autoes.Add(auto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(auto);
+        }
+
+        // GET: Car/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auto auto = db.Autoes.Find(id);
+            if (auto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auto);
+        }
+
+        // POST: Car/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Autonummer,Merk,Kenteken,Model,Brandstof,Kmstand,Kostperdag,Foto")] Auto auto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(auto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(auto);
+        }
+
+        // GET: Car/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auto auto = db.Autoes.Find(id);
+            if (auto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auto);
+        }
+
+        // POST: Car/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Auto auto = db.Autoes.Find(id);
+            db.Autoes.Remove(auto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
